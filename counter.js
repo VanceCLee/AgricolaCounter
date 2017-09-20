77ced0f4fd336a3a6dee4e9c347a2365497f7a63
Tevis Dryden <TevisDryden@gmail.com>
2017-09-19 22:37:32 -0600

    ids

---
+++ counter.js
@@ -0,0 +1,79 @@
+var fields1 = document.getElementById('fields1');
+var fields2 = document.getElementById('fields2');
+var fields3 = document.getElementById('fields3');
+var fields4 = document.getElementById('fields4');
+
+var pastures1 = document.getElementById('pastures1');
+var pastures2 = document.getElementById('pastures2');
+var pastures3 = document.getElementById('pastures3');
+var pastures4 = document.getElementById('pastures4');
+
+var grain1 = document.getElementById('grain1');
+var grain2 = document.getElementById('grain2');
+var grain3 = document.getElementById('grain3');
+var grain4 = document.getElementById('grain4');
+
+var vegetables1 = document.getElementById('vegetables1');
+var vegetables2 = document.getElementById('vegetables2');
+var vegetables3 = document.getElementById('vegetables3');
+var vegetables4 = document.getElementById('vegetables4');
+
+var sheep1 = document.getElementById('sheep1');
+var sheep2 = document.getElementById('sheep2');
+var sheep3 = document.getElementById('sheep3');
+var sheep4 = document.getElementById('sheep4');
+
+var wildBoar1 = document.getElementById('wildBoar1');
+var wildBoar2 = document.getElementById('wildBoar2');
+var wildBoar3 = document.getElementById('wildBoar3');
+var wildBoar4 = document.getElementById('wildBoar4');
+
+var cattle1 = document.getElementById('cattle1');
+var cattle2 = document.getElementById('cattle2');
+var cattle3 = document.getElementById('cattle3');
+var cattle4 = document.getElementById('cattle4');
+
+var unusedSpaces1 = document.getElementById('unusedSpaces1');
+var unusedSpaces2 = document.getElementById('unusedSpaces2');
+var unusedSpaces3 = document.getElementById('unusedSpaces3');
+var unusedSpaces4 = document.getElementById('unusedSpaces4');
+
+var fencedStables1 = document.getElementById('fencedStables1');
+var fencedStables2 = document.getElementById('fencedStables2');
+var fencedStables3 = document.getElementById('fencedStables3');
+var fencedStables4 = document.getElementById('fencedStables4');
+
+var clayRooms1 = document.getElementById('clayRooms1');
+var clayRooms2 = document.getElementById('clayRooms2');
+var clayRooms3 = document.getElementById('clayRooms3');
+var clayRooms4 = document.getElementById('clayRooms4');
+
+var stoneRooms1 = document.getElementById('stoneRooms1');
+var stoneRooms2 = document.getElementById('stoneRooms2');
+var stoneRooms3 = document.getElementById('stoneRooms3');
+var stoneRooms4 = document.getElementById('stoneRooms4');
+
+var familyMembers1 = document.getElementById('familyMembers1');
+var familyMembers2 = document.getElementById('familyMembers2');
+var familyMembers3 = document.getElementById('familyMembers3');
+var familyMembers4 = document.getElementById('familyMembers4');
+
+var cardPoints1 = document.getElementById('cardPoints1');
+var cardPoints2 = document.getElementById('cardPoints2');
+var cardPoints3 = document.getElementById('cardPoints3');
+var cardPoints4 = document.getElementById('cardPoints4');
+
+var bonusPoints1 = document.getElementById('bonusPoints1');
+var bonusPoints2 = document.getElementById('bonusPoints2');
+var bonusPoints3 = document.getElementById('bonusPoints3');
+var bonusPoints4 = document.getElementById('bonusPoints4');
+
+
+function addPlayer() {
+
+}
+
+function addScore(element) {
+	var id = element.id;
+
+}
