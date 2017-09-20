bdf54fe213d2ecb7ffd7ff5af7b6569c408d5126
Tevis Dryden <TevisDryden@gmail.com>
2017-09-20 14:54:51 -0600

    working on adding player functions

--- counter.js
+++ counter.js
@@ -1,9 +1,18 @@
-function addPlayer() {
+var players = 2;
 
+function addPlayer() {
+	if (players < 4) {
+
+	}
+}
+
+function activatePlayer() {
+	var elements = inputsArray(col);
+	
 }
 
 function clearCell(id) {
-	if id.value = "0" {
+	if (id.value == "0") {
 		id.value = "";
 	}
 }
@@ -16,22 +25,43 @@ function validate(id, col) {
 }
 
 function addScore(col) {
-	var fields = parseInt(document.getElementById('fields' + col).value);
-	var pastures = parseInt(document.getElementById('pastures' + col).value);
-	var grain = parseInt(document.getElementById('grain' + col).value);
-	var vegetables = parseInt(document.getElementById('vegetables' + col).value);
-	var sheep = parseInt(document.getElementById('sheep' + col).value);
-	var wildBoar = parseInt(document.getElementById('wildBoar' + col).value);
-	var cattle = parseInt(document.getElementById('cattle' + col).value);
-	var unusedSpaces = parseInt(document.getElementById('unusedSpaces' + col).value);
-	var fencedStables = parseInt(document.getElementById('fencedStables' + col).value);
-	var clayRooms = parseInt(document.getElementById('clayRooms' + col).value);
-	var stoneRooms = parseInt(document.getElementById('stoneRooms' + col).value);
-	var familyMembers = parseInt(document.getElementById('familyMembers' + col).value);
-	var cardPoints = parseInt(document.getElementById('cardPoints' + col).value);
-	var bonusPoints = parseInt(document.getElementById('bonusPoints' + col).value);
-	var total = document.getElementById('total' + col);
+	var elements = inputsArray(col);
+	var fields = parseInt(elements['fields'].value);
+	var pastures = parseInt(elements['pastures'].value);
+	var grain = parseInt(elements['grain'].value);
+	var vegetables = parseInt(elements['vegetables'].value);
+	var sheep = parseInt(elements['sheep'].value);
+	var wildBoar = parseInt(elements['wildBoar'].value);
+	var cattle = parseInt(elements['cattle'].value);
+	var unusedSpaces = parseInt(elements['unusedSpaces'].value);
+	var fencedStables = parseInt(elements['fencedStables'].value);
+	var clayRooms = parseInt(elements['clayRooms'].value);
+	var stoneRooms = parseInt(elements['stoneRooms'].value);
+	var familyMembers = parseInt(elements['familyMembers'].value);
+	var cardPoints = parseInt(elements['cardPoints'].value);
+	var bonusPoints = parseInt(elements['bonusPoints'].value);
 
 	var score = fields + pastures + grain + vegetables + sheep + wildBoar + cattle + unusedSpaces + fencedStables + clayRooms + stoneRooms + familyMembers + cardPoints + bonusPoints;
-	total.innerHTML = score;
+	elements['total'].innerHTML = score;
+}
+
+function inputsArray(col) {
+	var elements = [];
+	elements["fields"] = document.getElementById('fields' + col);
+	elements["pastures"] = document.getElementById('pastures' + col);
+	elements["grain"] = document.getElementById('grain' + col);
+	elements["vegetables"] = document.getElementById('vegetables' + col);
+	elements["sheep"] = document.getElementById('sheep' + col);
+	elements["wildBoar"] = document.getElementById('wildBoar' + col);
+	elements["cattle"] = document.getElementById('cattle' + col);
+	elements["unusedSpaces"] = document.getElementById('unusedSpaces' + col);
+	elements["fencedStables"] = document.getElementById('fencedStables' + col);
+	elements["clayRooms"] = document.getElementById('clayRooms' + col);
+	elements["stoneRooms"] = document.getElementById('stoneRooms' + col);
+	elements["familyMembers"] = document.getElementById('familyMembers' + col);
+	elements["cardPoints"] = document.getElementById('cardPoints' + col);
+	elements["bonusPoints"] = document.getElementById('bonusPoints' + col);
+	elements["total"] = document.getElementById('total' + col);
+
+	return elements;
 }
